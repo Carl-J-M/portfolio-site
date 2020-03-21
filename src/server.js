abd2e08dcc886a202931ab73f66d46e9f0e0d1d3
Carl-J-M <carlmensahmail@gmail.com>
2020-03-21 20:30:08 +0000

    Added express as dependency

--- src/server.js
+++ src/server.js
@@ -5,11 +5,11 @@ const path = require('path')
 const app = express()
 
 //here we are configuring dist to serve app files
-app.use('/', serveStatic(path.join(__dirname, '/dist/portfolio-site')))
+app.use('/', serveStatic(path.join(__dirname, './dist/portfolio-site')))
 
 // this * route is to serve project on different page routes except root `/`
 app.get(/.*/, function (req, res) {
-	res.sendFile(path.join(__dirname, '/dist/portfolio-site/index.html'))
+	res.sendFile(path.join(__dirname, './dist/portfolio-site/index.html'))
 })
 
 const port = process.env.PORT || 8080
